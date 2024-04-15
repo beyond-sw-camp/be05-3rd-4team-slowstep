2160a4c39bf19dd08a65674fe513e104da2167b0
GGOBUGl <tjrmsduq@naver.com>
2024-04-15 17:03:26 +0900

    exam add, view

--- src/pages/Patient/ExamAddPage.vue
+++ src/pages/Patient/ExamAddPage.vue
@@ -1,15 +1,106 @@
 <template>
-  <div>
-    MsgListPage
+  <div class="container">
+    <div class="row mb-2 mt-5">
+      <div class="col-4">
+        <div class="fromDiv">
+          <div><h2>진료기록 입력</h2></div>
+          <div><h2>김점순 환자</h2></div>
+        </div>
+      </div>
+      <div class="col-5">
+        <div class="input-group">
+          <span class="input-group-text">기록날짜</span>
+          <input
+            type="text"
+            aria-label="Year"
+            class="form-control indent"
+            placeholder="년"
+          />
+          <input
+            type="text"
+            aria-label="Month"
+            class="form-control indent"
+            placeholder="월"
+          />
+          <input
+            type="text"
+            aria-label="Day"
+            class="form-control indent"
+            placeholder="일"
+          />
+        </div>
+      </div>
+
+      <div class="col-3"></div>
+    </div>
+    <!-- 년, 월, 일 입력란 종료 -->
+
+    <div class="row mb-2">
+      <div class="col-4">
+        <div class="fromDiv"></div>
+      </div>
+      <div class="col-5">
+        <div class="input-group">
+          <span class="input-group-text">특이사항</span>
+          <textarea
+            class="form-control"
+            aria-label="With textarea"
+            rows="10"
+          ></textarea>
+        </div>
+      </div>
+
+      <div class="col-3"></div>
+    </div>
+    <!-- 특이사항 입력란 종료 -->
+
+    <div class="row mb-2 mt-5">
+      <div class="col-4">
+        <div class="fromDiv"></div>
+      </div>
+
+      <div class="col-5">
+        <div class="input-group">
+          <span class="input-group-text">약물처방</span>
+          <textarea
+            class="form-control"
+            aria-label="With textarea"
+            rows="5"
+          ></textarea>
+        </div>
+      </div>
+
+      <div class="col-3"></div>
+    </div>
+    <div class="row mb-2 mt-5">
+      <div class="col-4"></div>
+      <div class="col-5 but">
+        <button class="btn btn-success back">뒤로가기</button>
+        <button class="btn btn-success regi">등록</button>
+      </div>
+
+      <div class="col-3"></div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style scoped>
+.indent {
+}
+.back {
+  position: absolute;
+  margin-left: 160px;
+}
+.regi {
+  margin-left: 290px;
+  position: absolute;
+}
 
+.fromDiv {
+  border: none;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,77 +1,78 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import Login from '@/pages/User/LoginPage.vue'
-import UserMain from '@/pages/User/MainPage.vue'
-import MsgList from '@/pages/Msg/MsgListPage.vue'
-import MsgAdd from '@/pages/Msg/MsgAddPage.vue'
-import MsgView from '@/pages/Msg/MsgViewPage.vue'
-import PatientMain from '@/pages/Patient/PatientMain.vue'
-import HealthList from '@/pages/Patient/HealthListPage.vue'
-import HealthView from '@/pages/Patient/HealthViewPage.vue'
-import HealthAdd from '@/pages/Patient/HealthAddPage.vue'
-import ExamView from '@/pages/Patient/ExamViewPage.vue'
+import Login from "@/pages/User/LoginPage.vue";
+import UserMain from "@/pages/User/MainPage.vue";
+import MsgList from "@/pages/Msg/MsgListPage.vue";
+import MsgAdd from "@/pages/Msg/MsgAddPage.vue";
+import MsgView from "@/pages/Msg/MsgViewPage.vue";
+import PatientMain from "@/pages/Patient/PatientMain.vue";
+import HealthList from "@/pages/Patient/HealthListPage.vue";
+import HealthView from "@/pages/Patient/HealthViewPage.vue";
+import HealthAdd from "@/pages/Patient/HealthAddPage.vue";
+import ExamAdd from "@/pages/Patient/ExamAddPage.vue";
+import ExamView from "@/pages/Patient/ExamViewpage.vue";
 
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "Login",
+      component: Login,
+    },
+    {
+      path: "/user/main",
+      name: "UserMain",
+      component: UserMain,
+    },
+    {
+      path: "/msg/list",
+      name: "MsgList",
+      component: MsgList,
+    },
+    {
+      path: "/msg/add",
+      name: "MsgAdd",
+      component: MsgAdd,
+    },
+    {
+      path: "/msg/view",
+      name: "MsgView",
+      component: MsgView,
+    },
+    {
+      path: "/patient/main",
+      name: "PatientMain",
+      component: PatientMain,
+    },
+    {
+      path: "/patient/health/list",
+      name: "HealthList",
+      component: HealthList,
+    },
+    {
+      path: "/patient/health/view/:id",
+      name: "HealthView",
+      component: HealthView,
+    },
+    {
+      path: "/patient/health/add",
+      name: "HealthAdd",
+      component: HealthAdd,
+    },
 
+    {
+      path: "/patient/exam/add",
+      name: "ExamAdd",
+      component: ExamAdd,
+    },
 
-
-const router = createRouter( {
-    history: createWebHistory(),
-    routes: [
-        {
-            path : '/',
-            name : 'Login',
-            component : Login
-        },
-        {
-            path : '/user/main',
-            name : 'UserMain',
-            component : UserMain
-        },
-        {
-            path : '/msg/list',
-            name : 'MsgList',
-            component : MsgList
-        },
-        {
-            path : '/msg/add',
-            name : 'MsgAdd',
-            component : MsgAdd
-        },
-        {
-            path : '/msg/view/:id',
-            name : 'MsgView',
-            component : MsgView
-        },
-        {
-            path : '/patient/main',
-            name : 'PatientMain',
-            component : PatientMain
-        },   
-        {
-            path : '/patient/health/list',
-            name : 'HealthList',
-            component : HealthList
-
-        },
-        {
-            path : '/patient/health/view',
-            name : 'HealthView',
-            component : HealthView
-        },
-        {
-            path : '/patient/health/add',
-            name : 'HealthAdd',
-            component : HealthAdd
-        },
-        {
-            path : '/patient/exam/view/:id',
-            name : 'ExamView',
-            component : ExamView
-        }
-        
-       
-    ]
+    {
+      path: "/patient/exam/view",
+      name: "ExamView",
+      component: ExamView,
+    },
+  ],
 });
-
 
 export default router;
